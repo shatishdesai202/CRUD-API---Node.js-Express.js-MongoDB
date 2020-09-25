00cf0f62beeb8cca739ca609a93a1a350e9b8a07
Shatish Desai <sdondjango@gmail.com>
2020-09-25 14:02:16 +0530

    Header Set

--- routes/urls.js
+++ routes/urls.js
@@ -9,6 +9,13 @@ router.get('/', async (req, res) => {
     try {
 
         const alldata = await data.find();
+        
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials",true);
+        res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
+
+
         res.json(alldata);
 
     } catch (error) {
@@ -33,6 +40,13 @@ router.post('/signup', async (req, res) => {
     try {
         // res.send('ok');
         const showDatas = await newData.save();
+
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials",true);
+        res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
+
+
         res.json(showDatas);
     } catch (error) {
         res.send("Error In Post -> " + error);
@@ -44,6 +58,11 @@ router.get('/getbyid/:id', async (req, res) => {
 
     try {
         const showdata = await data.findById(req.params.id);
+
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials",true);
+        res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
         
         res.json(showdata);
     } catch (error) {
@@ -57,6 +76,12 @@ router.patch('/update/:id', async (req, res)=>{
     try {
         const updateData = await data.findByIdAndUpdate(req.params.id, {name: req.body.name, surname: req.body.surname, age: req.body.age} );
         updateData.save();
+
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials",true);
+        res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
+
         const x = await data.findById(req.params.id);
         res.json(x);
     } catch (error) {
@@ -72,6 +97,12 @@ router.delete('/delete/:id',  async(req, res)=>{
     try {
         const deleteData = await data.findByIdAndDelete(ids);
         // deleteData.save()
+
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials",true);
+        res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
+
         res.json('data deleted');
     } catch (error) {
         res.send("ERROR "+error);
